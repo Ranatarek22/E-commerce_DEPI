f1d879f2345cd55a1ca016c1bf6e30f3bc495ae3
Nada Adel <120988662+nadaadel50@users.noreply.github.com>
2024-10-14 22:06:53 +0300

    Update ProductItem.jsx

--- src/component/ProductItem.jsx
+++ src/component/ProductItem.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 const ProductItem = ({id, image, name, price}) => {
   return (
     <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
-        <div className='overflow-hidden'>
-            <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt=''/>
+        <div className='w-full h-64 overflow-hidden'>
+            <img className='w-full h-full object-cover hover:scale-110 transition ease-in-out duration-300' src={image} alt={name}/>
         </div>
         <p className='pt-3 pb-1 text-sm'>{name}</p>
         <p className='text-sm font-medium'>${price}</p>
