4d182d2befa84ef4db3a5cd4b834cab34e49c0c7
Nada Adel <120988662+nadaadel50@users.noreply.github.com>
2024-10-14 22:10:06 +0300

    Update Collection.jsx

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,109 +1,92 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from '../context/ShopContext'
+import React, { useContext, useEffect, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
 import { assets } from '../frontend_assets/assets';
 import ProductItem from '../component/ProductItem';
 
 const Collection = () => {
-  const {products, search, setSearch, showSearch, setShowSearch} = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
   const [typeProducts, setTypeProducts] = useState([]);
   const [sortType, setSortType] = useState();
 
-  const categoriesSelected = (e)=>{
-    if(category.includes(e.target.value)){
+  const categoriesSelected = (e) => {
+    if (category.includes(e.target.value)) {
       setCategory(category.filter((item) => item !== e.target.value));
-    }
-    else{
+    } else {
       setCategory([...category, e.target.value]);
     }
-  }
+  };
 
-  const typeSelected = (e)=>{
-    if(typeProducts.includes(e.target.value)){
+  const typeSelected = (e) => {
+    if (typeProducts.includes(e.target.value)) {
       setTypeProducts(typeProducts.filter((item) => item !== e.target.value));
-    }
-    else{
+    } else {
       setTypeProducts([...typeProducts, e.target.value]);
     }
-  }
+  };
 
-  const applyFilters = ()=>{
+  const applyFilters = () => {
     let productsCopy = [...products];
 
-    if(search && showSearch){
-      productsCopy = productsCopy.filter((item)=>{
-        return item.name.toUpperCase().includes(search.toUpperCase());
-      })
+    if (search && showSearch) {
+      productsCopy = productsCopy.filter((item) => {
+        return item.title.toUpperCase().includes(search.toUpperCase());
+      });
     }
 
-    if(category.length > 0){
-      productsCopy = productsCopy.filter(item=> category.includes(item.category));
+    if (category.length > 0) {
+      productsCopy = productsCopy.filter((item) => category.includes(item.category));
     }
-    if(typeProducts.length > 0){
-      productsCopy = productsCopy.filter(item=> typeProducts.includes(item.subCategory));
+
+    if (typeProducts.length > 0) {
+      productsCopy = productsCopy.filter((item) => typeProducts.includes(item.subCategory));
     }
+
     setFilterProducts(productsCopy);
-  }
-  
-  const sortProducts = ()=>{
+  };
+
+  const sortProducts = () => {
     let filteredProductsCopy = [...filterProducts];
 
-    if(sortType == 'low-high'){
-      setFilterProducts(filteredProductsCopy.sort((a, b)=> a.price - b.price));
-    }
-    else if(sortType == 'high-low'){
-      setFilterProducts(filteredProductsCopy.sort((b,a)=>a.price - b.price));
-    }
-    else{
+    if (sortType === 'low-high') {
+      setFilterProducts(filteredProductsCopy.sort((a, b) => a.price - b.price));
+    } else if (sortType === 'high-low') {
+      setFilterProducts(filteredProductsCopy.sort((a, b) => b.price - a.price));
+    } else {
       applyFilters();
     }
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     applyFilters();
     setSortType('relevant');
-  },[category,typeProducts, search, showSearch])
+  }, [category, typeProducts, search, showSearch]);
 
-  useEffect(()=>{
+  useEffect(() => {
     sortProducts();
-  },[sortType])
+  }, [sortType]);
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
       {/* Filter Options */}
       <div className='min-w-60'>
-        <p onClick={()=>setShowFilter(!showFilter)} className='my-2 font-bold text-xl flex items-center cursor-pointer gap-2'>FILTERS
-          <img className={`h-3 sm:hidden ${showFilter? 'rotate-90': ''}`} src={assets.dropdown_icon} alt='' />
+        <p onClick={() => setShowFilter(!showFilter)} className='my-2 font-bold text-xl flex items-center cursor-pointer gap-2'>FILTERS
+          <img className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`} src={assets.dropdown_icon} alt='' />
         </p>
 
-        <div className={`border border-gray-300 pl-5 py-3 mt-6 sm:block ${showFilter ? '': 'hidden'}`}>
+        <div className={`border border-gray-300 pl-5 py-3 mt-6 sm:block ${showFilter ? '' : 'hidden'}`}>
           <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className='flex gap-2'>
-              <input type='checkbox' value={'Men'} onChange={categoriesSelected}/> Men
+              <input type='checkbox' value={"Clothing"} onChange={categoriesSelected} /> Clothing
             </p>
             <p className='flex gap-2'>
-              <input type='checkbox' value={'Women'} onChange={categoriesSelected}/> Women
+              <input type='checkbox' value={"Jewelery"} onChange={categoriesSelected} /> Jewelery
             </p>
             <p className='flex gap-2'>
-              <input type='checkbox' value={'Kids'} onChange={categoriesSelected}/> Kids
-            </p>
-          </div>
-        </div>
-
-        <div className={`border border-gray-300 pl-5 py-3 my-5 sm:block ${showFilter ? '': 'hidden'}`}>
-          <p className='mb-3 text-sm font-medium'>TYPE</p>
-          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
-            <p className='flex gap-2'>
-              <input type='checkbox' value={'Topwear'} onChange={typeSelected}/> Topwear
-            </p>
-            <p className='flex gap-2'>
-              <input type='checkbox' value={'Bottomwear'} onChange={typeSelected}/> Bottomwear
-            </p>
-            <p className='flex gap-2'>
-              <input type='checkbox' value={'Winterwear'} onChange={typeSelected}/> Winterwear
+              <input type='checkbox' value={"Electronics"} onChange={categoriesSelected} /> Electronics
             </p>
           </div>
         </div>
@@ -115,25 +98,23 @@ const Collection = () => {
           <p className='font-bold'>ALL COLLECTIONS</p>
 
           {/* SORTING */}
-          <select value={sortType} onChange={(e)=>setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2 rounded-lg'>
+          <select value={sortType} onChange={(e) => setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2 rounded-lg'>
             <option value='relevant'>Sort: relevant</option>
-            <option value='low-high'>Sort: low-high </option>
+            <option value='low-high'>Sort: low-high</option>
             <option value='high-low'>Sort: high-low</option>
           </select>
         </div>
 
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
           {
-            filterProducts.map((item, index)=>(
-              <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+            filterProducts.map((item, index) => (
+              <ProductItem key={index} id={item.id} image={item.image} name={item.title} price={item.price} />
             ))
           }
         </div>
-
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Collection
+export default Collection;
